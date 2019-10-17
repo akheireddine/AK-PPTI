2381bb430176199dd3e579e225ff2769af273de7
Anissa Kheireddine <akheireddine@nuc2.lrde.epita.fr>
2019-10-17 17:35:35 +0200

    plan

---
+++ model.py
@@ -0,0 +1,26 @@
+
+
+def read_InPut_file():
+    pass
+
+def write_OutPut_file():
+    pass
+
+def Solve_Model():
+    pass
+
+
+
+
+
+if __name__ == '__main__':
+    
+    read_InPut_file()
+    
+    
+    Solve_Model()
+    
+    
+    write_OutPut_file()
+    
+
